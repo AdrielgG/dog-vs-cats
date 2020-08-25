bf529b4bdfd58886ea8141a4bd6d9bd97feaeba3
AdrielgG <65690462+AdrielgG@users.noreply.github.com>
2020-08-25 12:18:30 -0300

    Create prediction.py

---
+++ prediction.py
@@ -0,0 +1,13 @@
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('/content/dataset/single_prediction/cat.74.jpg', target_size = (64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+result = classifier.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+  prediction = 'dog'
+  print(prediction)
+if result[0][0] == 0:
+  prediction = 'cat'
+  print(prediction)
